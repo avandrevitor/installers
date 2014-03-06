1cb9753759e66f52b031d0f8f90b120eeda228ec
André Vitor <avandrevitor@gmail.com>
2014-03-06 12:08:38 -0300

    Refatoring project, now testing use vagrant

---
+++ tests/basic.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get autoclean
+sudo apt-get install oracle-java7-installer -y
+sudo apt-get install git-core vim -y
