8a595bd87496751e288f637222a53d9819678e7f
André Vitor <avandrevitor@gmail.com>
2014-03-10 15:34:05 -0300

    Reusing Installer Reference: https://github.com/petems/puppet-install-shell.git

---
+++ installers/puppet-agent/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#############################################################################################
+#
+#	Debian/Ubuntu Installer Puppet Agent 
+#
+#	reference http://www.anggrianto.com/blog/install-puppet-and-puppet-master-on-ubuntu/
+#
+##############################################################################################
+
+# Root ? 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Change to use with Vagrant
+# sudo -i
+
+DEBIAN_FRONTEND=noninteractive git clone https://github.com/petems/puppet-install-shell.git
+DEBIAN_FRONTEND=noninteractive sh ./puppet-install-shell/install_puppet.sh
+
